189cf26e9936bbb16e28854c9de446bf92079c13
TheEvilSkeleton <theevilskeleton@riseup.net>
2022-08-02 15:09:52 -0400

    Use pascal case

--- src/dialogs/vkbasalt.py
+++ src/dialogs/vkbasalt.py
@@ -32,7 +32,7 @@ from bottles.backend.utils.manager import ManagerUtils
 from bottles.dialogs.filechooser import FileChooser  # pyright: reportMissingImports=false
 import logging
 
-class vkbasalt_settings:
+class VkBasaltSettings:
     default = False
     effects = False
     output = False
@@ -105,55 +105,55 @@ class VkBasaltDialog(Adw.Window):
         self.btn_lut_reset.connect("clicked", self.__reset_clut)
 
         if os.path.isfile(conf):
-            vkbasalt_settings = ParseConfig(conf)
+            VkBasaltSettings = ParseConfig(conf)
 
             # Check if effects are used.
-            if vkbasalt_settings.effects:
-                if "cas" not in vkbasalt_settings.effects:
+            if VkBasaltSettings.effects:
+                if "cas" not in VkBasaltSettings.effects:
                     self.expander_cas.set_enable_expansion(False)
-                if "dls" not in vkbasalt_settings.effects:
+                if "dls" not in VkBasaltSettings.effects:
                     self.expander_dls.set_enable_expansion(False)
-                if "fxaa" not in vkbasalt_settings.effects:
+                if "fxaa" not in VkBasaltSettings.effects:
                     self.expander_fxaa.set_enable_expansion(False)
-                if "smaa" not in vkbasalt_settings.effects:
+                if "smaa" not in VkBasaltSettings.effects:
                     self.expander_smaa.set_enable_expansion(False)
             else:
-                vkbasalt_settings.effects = False
+                VkBasaltSettings.effects = False
                 self.expander_cas.set_enable_expansion(False)
                 self.expander_dls.set_enable_expansion(False)
                 self.expander_fxaa.set_enable_expansion(False)
                 self.expander_smaa.set_enable_expansion(False)
 
             # Check if clut is used.
-            if vkbasalt_settings.lut_file_path is None:
+            if VkBasaltSettings.lut_file_path is None:
                 self.btn_lut_file_path = False
             else:
-                self.action_clut.set_subtitle(vkbasalt_settings.lut_file_path)
-                self.btn_lut_file_path = vkbasalt_settings.lut_file_path
+                self.action_clut.set_subtitle(VkBasaltSettings.lut_file_path)
+                self.btn_lut_file_path = VkBasaltSettings.lut_file_path
                 self.btn_lut_reset.show()
 
-            if vkbasalt_settings.cas_sharpness != None:
-                self.spin_cas_sharpness.set_value(float(vkbasalt_settings.cas_sharpness))
-            if vkbasalt_settings.dls_sharpness != None:
-                self.spin_dls_sharpness.set_value(float(vkbasalt_settings.dls_sharpness))
-            if vkbasalt_settings.dls_denoise != None:
-                self.spin_dls_denoise.set_value(float(vkbasalt_settings.dls_denoise))
-            if vkbasalt_settings.fxaa_subpixel_quality != None:
-                self.spin_fxaa_subpixel_quality.set_value(float(vkbasalt_settings.fxaa_subpixel_quality))
-            if vkbasalt_settings.fxaa_quality_edge_threshold != None:
-                self.spin_fxaa_quality_edge_threshold.set_value(float(vkbasalt_settings.fxaa_quality_edge_threshold))
-            if vkbasalt_settings.fxaa_quality_edge_threshold_min != None:
-                self.spin_fxaa_quality_edge_threshold_min.set_value(float(vkbasalt_settings.fxaa_quality_edge_threshold_min))
-            if vkbasalt_settings.smaa_threshold != None:
-                self.spin_smaa_threshold.set_value(float(vkbasalt_settings.smaa_threshold))
-            if vkbasalt_settings.smaa_max_search_steps != None:
-                self.spin_smaa_max_search_steps.set_value(float(vkbasalt_settings.smaa_max_search_steps))
-            if vkbasalt_settings.smaa_max_search_steps_diagonal != None:
-                self.spin_smaa_max_search_steps_diagonal.set_value(float(vkbasalt_settings.smaa_max_search_steps_diagonal))
-            if vkbasalt_settings.smaa_corner_rounding != None:
-                self.spin_smaa_corner_rounding.set_value(float(vkbasalt_settings.smaa_corner_rounding))
-            if vkbasalt_settings.smaa_edge_detection != None:
-                if vkbasalt_settings.smaa_edge_detection == "color":
+            if VkBasaltSettings.cas_sharpness != None:
+                self.spin_cas_sharpness.set_value(float(VkBasaltSettings.cas_sharpness))
+            if VkBasaltSettings.dls_sharpness != None:
+                self.spin_dls_sharpness.set_value(float(VkBasaltSettings.dls_sharpness))
+            if VkBasaltSettings.dls_denoise != None:
+                self.spin_dls_denoise.set_value(float(VkBasaltSettings.dls_denoise))
+            if VkBasaltSettings.fxaa_subpixel_quality != None:
+                self.spin_fxaa_subpixel_quality.set_value(float(VkBasaltSettings.fxaa_subpixel_quality))
+            if VkBasaltSettings.fxaa_quality_edge_threshold != None:
+                self.spin_fxaa_quality_edge_threshold.set_value(float(VkBasaltSettings.fxaa_quality_edge_threshold))
+            if VkBasaltSettings.fxaa_quality_edge_threshold_min != None:
+                self.spin_fxaa_quality_edge_threshold_min.set_value(float(VkBasaltSettings.fxaa_quality_edge_threshold_min))
+            if VkBasaltSettings.smaa_threshold != None:
+                self.spin_smaa_threshold.set_value(float(VkBasaltSettings.smaa_threshold))
+            if VkBasaltSettings.smaa_max_search_steps != None:
+                self.spin_smaa_max_search_steps.set_value(float(VkBasaltSettings.smaa_max_search_steps))
+            if VkBasaltSettings.smaa_max_search_steps_diagonal != None:
+                self.spin_smaa_max_search_steps_diagonal.set_value(float(VkBasaltSettings.smaa_max_search_steps_diagonal))
+            if VkBasaltSettings.smaa_corner_rounding != None:
+                self.spin_smaa_corner_rounding.set_value(float(VkBasaltSettings.smaa_corner_rounding))
+            if VkBasaltSettings.smaa_edge_detection != None:
+                if VkBasaltSettings.smaa_edge_detection == "color":
                     self.toggle_color.set_active(True)
                     self.smaa_edge_detection = "color"
                 else:
@@ -176,17 +176,17 @@ class VkBasaltDialog(Adw.Window):
 
         # Applies default settings and closes dialog.
         if self.switch_default.get_state() is True:
-            vkbasalt_settings.default = True
-            vkbasalt_settings.output = False
+            VkBasaltSettings.default = True
+            VkBasaltSettings.output = False
             conf = os.path.join(conf, "vkBasalt.conf")
             if os.path.isfile(conf):
                 logging.info(f"Removing file: {conf}")
                 os.remove(conf)
-            parse(vkbasalt_settings)
+            parse(VkBasaltSettings)
             self.close()
             return GLib.SOURCE_REMOVE
         else:
-            vkbasalt_settings.default = False
+            VkBasaltSettings.default = False
 
         # Checks filter settings.
         if True in [
@@ -198,35 +198,35 @@ class VkBasaltDialog(Adw.Window):
             effects = []
             if self.expander_cas.get_enable_expansion() is True:
                 effects.append("cas")
-                vkbasalt_settings.cas_sharpness = Gtk.Adjustment.get_value(self.spin_cas_sharpness)
+                VkBasaltSettings.cas_sharpness = Gtk.Adjustment.get_value(self.spin_cas_sharpness)
             if self.expander_dls.get_enable_expansion() is True:
                 effects.append("dls")
-                vkbasalt_settings.dls_sharpness = Gtk.Adjustment.get_value(self.spin_dls_sharpness)
-                vkbasalt_settings.dls_denoise = Gtk.Adjustment.get_value(self.spin_dls_denoise)
+                VkBasaltSettings.dls_sharpness = Gtk.Adjustment.get_value(self.spin_dls_sharpness)
+                VkBasaltSettings.dls_denoise = Gtk.Adjustment.get_value(self.spin_dls_denoise)
             if self.expander_fxaa.get_enable_expansion() is True:
                 effects.append("fxaa")
-                vkbasalt_settings.fxaa_subpixel_quality = Gtk.Adjustment.get_value(self.spin_fxaa_subpixel_quality)
-                vkbasalt_settings.fxaa_quality_edge_threshold = Gtk.Adjustment.get_value(self.spin_fxaa_quality_edge_threshold)
-                vkbasalt_settings.fxaa_quality_edge_threshold_min = Gtk.Adjustment.get_value(self.spin_fxaa_quality_edge_threshold_min)
+                VkBasaltSettings.fxaa_subpixel_quality = Gtk.Adjustment.get_value(self.spin_fxaa_subpixel_quality)
+                VkBasaltSettings.fxaa_quality_edge_threshold = Gtk.Adjustment.get_value(self.spin_fxaa_quality_edge_threshold)
+                VkBasaltSettings.fxaa_quality_edge_threshold_min = Gtk.Adjustment.get_value(self.spin_fxaa_quality_edge_threshold_min)
             if self.expander_smaa.get_enable_expansion() is True:
                 effects.append("smaa")
-                vkbasalt_settings.smaa_threshold = Gtk.Adjustment.get_value(self.spin_smaa_threshold)
-                vkbasalt_settings.smaa_edge_detection = self.smaa_edge_detection
-                vkbasalt_settings.smaa_corner_rounding = Gtk.Adjustment.get_value(self.spin_smaa_corner_rounding)
-                vkbasalt_settings.smaa_max_search_steps = Gtk.Adjustment.get_value(self.spin_smaa_max_search_steps)
-                vkbasalt_settings.smaa_max_search_steps_diagonal = Gtk.Adjustment.get_value(self.spin_smaa_max_search_steps_diagonal)
+                VkBasaltSettings.smaa_threshold = Gtk.Adjustment.get_value(self.spin_smaa_threshold)
+                VkBasaltSettings.smaa_edge_detection = self.smaa_edge_detection
+                VkBasaltSettings.smaa_corner_rounding = Gtk.Adjustment.get_value(self.spin_smaa_corner_rounding)
+                VkBasaltSettings.smaa_max_search_steps = Gtk.Adjustment.get_value(self.spin_smaa_max_search_steps)
+                VkBasaltSettings.smaa_max_search_steps_diagonal = Gtk.Adjustment.get_value(self.spin_smaa_max_search_steps_diagonal)
 
-            vkbasalt_settings.effects = tuple(effects)
+            VkBasaltSettings.effects = tuple(effects)
 
         else:
-            vkbasalt_settings.effects = False
+            VkBasaltSettings.effects = False
 
         if self.btn_lut_file_path:
-            vkbasalt_settings.lut_file_path = self.btn_lut_file_path
+            VkBasaltSettings.lut_file_path = self.btn_lut_file_path
 
-        vkbasalt_settings.output = conf
+        VkBasaltSettings.output = conf
 
-        parse(vkbasalt_settings)
+        parse(VkBasaltSettings)
         self.close()
         return GLib.SOURCE_REMOVE
 
@@ -334,7 +334,7 @@ class VkBasaltDialog(Adw.Window):
 
     def __reset_clut(self, *args):
         self.btn_lut_file_path = False
-        vkbasalt_settings.lut_file_path = False
+        VkBasaltSettings.lut_file_path = False
         self.btn_lut_reset.hide()
         self.action_clut.set_subtitle(self.__default_lut_msg)
         self.__check_state()
